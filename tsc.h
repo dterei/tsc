d1fa999a8d203b308fb7dc7d9bbe0a9a71d74948
David Terei <code@davidterei.com>
2016-06-22 23:40:03 -0700

    Fix compiler warning + doc functions

--- tsc.h
+++ tsc.h
@@ -10,11 +10,11 @@
 
 #define TSC_OVERHEAD_N 100000
 
+// bench_start returns a timestamp for use to measure the start of a benchmark
+// run.
 static inline uint64_t bench_start(void)
 {
   unsigned  cycles_low, cycles_high;
-  uint64_t t;
-
   asm volatile( "CPUID\n\t" // serialize
                 "RDTSC\n\t" // read clock
                 "mov %%edx, %0\n\t"
@@ -24,11 +24,10 @@ static inline uint64_t bench_start(void)
   return ((uint64_t) cycles_high << 32) | cycles_low;
 }
 
+// bench_end returns a timestamp for use to measure the end of a benchmark run.
 static inline uint64_t bench_end(void)
 {
   unsigned  cycles_low, cycles_high;
-  uint64_t t;
-
   asm volatile( "RDTSCP\n\t" // read clock + serialize
                 "mov %%edx, %0\n\t"
                 "mov %%eax, %1\n\t"
@@ -38,6 +37,8 @@ static inline uint64_t bench_end(void)
   return ((uint64_t) cycles_high << 32) | cycles_low;
 }
 
+// measure_tsc_overhead returns the overhead from benchmarking, it should be
+// subtracted from timings to improve accuracy.
 static uint64_t measure_tsc_overhead(void)
 {
   uint64_t t0, t1, overhead = ~0;
